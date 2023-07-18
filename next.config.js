8acdb470d0e8428d40a3e41fa069614a2ea110e8
jwzzang12 <moonbb12@gmail.com>
2023-07-18 13:31:34 +0100

    Redirects / Rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+const API_KEY = 'd7d6b436c2aafbb903452db6223d9e66';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  //URL change
+  async redirects() {
+    return [
+      {
+        source: '/old-blog/:path',
+        destination: '/new-blog/:path',
+        permanent: false,
+      },
+    ];
+  },
+  //URL not change
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import Layout from '@/components/Layout';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+        .active {
+          color: tomato;
+        }
+      `}</style>
+    </Layout>
+  );
 }
